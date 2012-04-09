05eedac7a90bcfca25a8331577538e58e9db7031
Thiago Azevedo Alves <taalves@inf.ufpel.edu.br>
2012-04-09 11:49:52 -0300

    Adicionando tratamento de sockets

--- servidor.cpp
+++ servidor.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -20,6 +22,17 @@ int main(int argc, char *argv[]){
 	int porta = atoi(argv[1]); //a porta em que o servidor irá executar
 	socklen_t cliente_len; //armazena o tamanho do endereço do cliente
 	char buffer[256];
-	struct sockaddr_in end_serv, end_cli; // contém o endereço do sevidor e cliente
-	cout << porta << endl;
+	struct sockaddr_in end_serv, end_cli;   // contém o endereço do sevidor e cliente
+	sock = socket(AF_INET, SOCK_STREAM, 0); //cria um novo socket
+	if (sock < 0){                          //retorna -1 no caso de falha
+		cout << "Erro ao abrir socket." << endl;
+	}
+	bzero((char*) &end_serv, sizeof(end_serv)); //inicia a estrutura com todos os valores zero
+	end_serv.sin_family = AF_INET;
+	end_serv.sin_addr.s_addr = INADDR_ANY;
+	end_serv.sin_port = htons(porta);
+	if (bind(sock, (struct sockaddr *) &end_serv, sizeof(end_serv)) < 0){ 
+		cout << "Erro ao realizar o bind." << endl;
+	}
+	listen(sock, 5);
 }
